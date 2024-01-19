92ce0ea1a495b867ad3734fdf6351bdbcc0af0f0
Ta9m1-N <149548001+Ta9m1-N@users.noreply.github.com>
2024-01-19 15:44:46 +0900

    No.31 Orders一覧 クエリの削減

--- src/main/java/com/example/repository/OrderRepository.java
+++ src/main/java/com/example/repository/OrderRepository.java
@@ -1,6 +1,8 @@
 package com.example.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 import com.example.model.Order;
 import java.util.List;
 
@@ -8,4 +10,7 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
 	List<Order> findByStatus(String status);
 
 	List<Order> findByPaymentStatusNot(String paymentStatus);
+
+	@Query(value = "SELECT o.id, o.customer_id, o.discount, o.shipping, o.tax, o.total, o.grand_total, o.status, o.payment_method, o.payment_status, o.paid, o.note, op.name, op.quantity FROM orders o INNER JOIN order_products op ON o.id = op.order_id", nativeQuery = true)
+	List<Object[]> findIndex();
 }
